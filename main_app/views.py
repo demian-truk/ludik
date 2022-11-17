ed9a901db012c019287ee73c76114b96fb5e6675
Dima Teteruk <dimtruk93@gmail.com>
2022-11-17 23:04:09 +0300

    Add index, about, feedback, rules and page_not_found views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,50 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic import FormView
 
-# Create your views here.
+from .forms import FeedbackForm
+from .models import Feedback
+
+
+def index(request):
+    return render(request, "main_app/index.html", {"title": "Ludik — портал про игры"})
+
+
+def about(request):
+    return render(request, "main_app/about.html", {"title": "О сайте"})
+
+
+class FeedbackFormView(FormView):
+    model = Feedback
+    template_name = "main_app/feedback.html"
+    form_class = FeedbackForm
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Обратная связь"
+        return dict(list(context.items()))
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.save()
+            messages.success(request, "Ваше сообщение отправлено")
+            return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+        else:
+            messages.error(request, "Ошибка отправки сообщения")
+
+        return render(request, self.template_name, self.get_context_data())
+
+
+def rules(request):
+    return render(
+        request, "main_app/rules.html", {"title": "Соглашение о пользовании сайтом"}
+    )
+
+
+def page_not_found(request, exception):
+    return render(
+        request, "main_app/page_not_found.html", {"title": "Страница не найдена"}
+    )
