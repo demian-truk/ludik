694e0ca055643e09fb2336ace56b1ee68b58e0c2
Dima Teteruk <dimtruk93@gmail.com>
2022-11-17 22:09:12 +0300

    Add games list and detail views

--- games/views.py
+++ games/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .filters import GamesFilter
+from .models import Game
+
+
+class GamesList(ListView):
+    model = Game
+    template_name = "games/games.html"
+    context_object_name = "games"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["filter"] = GamesFilter(self.request.GET, queryset=self.get_queryset())
+        context["title"] = "Игры"
+        return dict(list(context.items()))
+
+
+class ShowGame(DetailView):
+    model = Game
+    template_name = "games/show_game.html"
+    slug_url_kwarg = "game_slug"
+    context_object_name = "game"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = context["game"]
+        return dict(list(context.items()))
