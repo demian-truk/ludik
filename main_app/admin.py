6c7454e296bab4da8eaee722227baa80fbe4f152
Dima Teteruk <dimtruk93@gmail.com>
2022-11-17 22:58:04 +0300

    Add the "Feedback" and "Comment" models and register them in the admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Feedback
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "content", "time_create")
+    list_display_links = ("id", "name")
+    search_fields = ("name", "email", "content")
+    list_filter = ("time_create",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "news",
+        "user",
+        "time_create",
+        "content",
+        "publication_status",
+    )
+    list_display_links = ("id", "news")
+    search_fields = ("news__title", "user__username", "content")
+    list_filter = ("time_create", "publication_status")
+    raw_id_fields = ("user",)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from news.models import News
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Имя")
+    email = models.EmailField(verbose_name="Электронная почта")
+    content = models.TextField(max_length=1000, verbose_name="Текст обращения")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+
+    def __str__(self):
+        return "Обращение пользователя: {}".format(self.email)
+
+    class Meta:
+        verbose_name = "Обращение"
+        verbose_name_plural = "Обратная связь"
+        ordering = ["-time_create"]
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Автор комментария"
+    )
+    news = models.ForeignKey(
+        News,
+        on_delete=models.CASCADE,
+        related_name="comments_news",
+        verbose_name="Заголовок новости",
+    )
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    content = models.TextField(max_length=1000, verbose_name="Содержимое")
+    publication_status = models.BooleanField(
+        default=False, verbose_name="Публикация на сайте"
+    )
+
+    def __str__(self):
+        return "Комментарий пользователя: {}".format(self.user)
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ["-time_create"]
