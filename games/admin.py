37e8c43d4284ac4a175eb138982da3f769d9c11a
Dima Teteruk <dimtruk93@gmail.com>
2022-11-17 22:05:14 +0300

    Add the "Game" model and register it in the admin panel

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Game
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "get_html_image",
+        "release_date",
+        "developer",
+        "publication_status",
+    )
+    list_display_links = ("id", "name")
+    search_fields = ("name", "developer")
+    list_filter = ("publication_status",)
+    prepopulated_fields = {"slug": ("name",)}
+    readonly_fields = ("get_html_image",)
+
+    def get_html_image(self, object):
+        if object.image:
+            return mark_safe(f'<img src="{object.image.url}" width=60>')
+
+    get_html_image.short_description = "Превью"
--- games/models.py
+++ games/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.urls import reverse
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(
+        max_length=255, blank=True, null=True, verbose_name="Название"
+    )
+    slug = models.CharField(
+        max_length=255,
+        unique=True,
+        db_index=True,
+        blank=True,
+        null=True,
+        verbose_name="Slug",
+    )
+    content = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="games/%Y/%m/%d/", blank=True, null=True, verbose_name="Изображение"
+    )
+    release_date = models.DateField(blank=True, null=True, verbose_name="Дата выхода")
+    developer = models.CharField(
+        max_length=255, blank=True, null=True, verbose_name="Разработчик"
+    )
+    publisher = models.CharField(
+        max_length=255, blank=True, null=True, verbose_name="Издатель"
+    )
+    trailer_url = models.URLField(
+        blank=True, null=True, verbose_name="Ссылка на трейлер"
+    )
+    stopgame_rating = models.DecimalField(
+        blank=True,
+        null=True,
+        decimal_places=1,
+        max_digits=3,
+        verbose_name="StopGame рейтинг",
+    )
+    stopgame_link = models.URLField(blank=True, null=True, verbose_name="StopGame URL")
+    publication_status = models.BooleanField(
+        default=False, verbose_name="Публикация на сайте"
+    )
+    tags = TaggableManager(blank=True, verbose_name="Теги")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("show_game", kwargs={"game_slug": self.slug})
+
+    class Meta:
+        verbose_name = "Игра"
+        verbose_name_plural = "Игры"
+        ordering = ["-release_date"]
