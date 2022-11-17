7cc83344f7f0b7a0d8133c207b3e67ba9e026ee3
Dima Teteruk <dimtruk93@gmail.com>
2022-11-17 22:23:54 +0300

    Add the "News" and "NewsCategory" models and register them in the admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from news.models import News, NewsCategory
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "get_html_image", "time_create", "category")
+    list_display_links = ("id", "title")
+    search_fields = ("title", "category__name")
+    list_filter = ("time_create",)
+    prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = ("get_html_image",)
+
+    def get_html_image(self, object):
+        if object.image:
+            return mark_safe(f'<img src="{object.image.url}" width=60>')
+
+    get_html_image.short_description = "Превью"
+
+
+@admin.register(NewsCategory)
+class NewsCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.urls import reverse
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.CharField(
+        max_length=255, unique=True, db_index=True, verbose_name="Slug"
+    )
+    content = models.TextField(verbose_name="Содержимое")
+    image = models.ImageField(upload_to="news/%Y/%m/%d/", verbose_name="Изображение")
+    video_url = models.URLField(blank=True, null=True, verbose_name="Ссылка на видео")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    category = models.ForeignKey(
+        "NewsCategory", on_delete=models.PROTECT, verbose_name="Категория"
+    )
+    tags = TaggableManager(blank=True, verbose_name="Теги")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("show_news", kwargs={"news_slug": self.slug})
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+        ordering = ["-time_create"]
+
+
+class NewsCategory(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название")
+    slug = models.CharField(
+        max_length=50, unique=True, db_index=True, verbose_name="Slug"
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("news_categories", kwargs={"category_slug": self.slug})
+
+    class Meta:
+        verbose_name = "Категория новостей"
+        verbose_name_plural = "Категории новостей"
