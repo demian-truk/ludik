265d8109d5ed52ac8576a3f893e19f7c705a078e
Dima Teteruk <dimtruk93@gmail.com>
2022-11-17 22:40:16 +0300

    Add the "Profile" model and register it in the admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "get_html_photo", "date_joined", "gender"]
+    list_display_links = ("id", "user")
+    search_fields = ("user__username",)
+    list_filter = ("date_joined",)
+    readonly_fields = ("get_html_photo",)
+    raw_id_fields = ("user",)
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f'<img src="{object.photo.url}" width=60>')
+
+    get_html_photo.short_description = "Аватар"
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,35 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+GENDER_CHOICES = (("мужской", "Мужской"), ("женский", "Женский"))
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    date_joined = models.DateTimeField(
+        default=datetime.now, verbose_name="Дата регистрации"
+    )
+    date_of_birth = models.DateField(
+        blank=True, null=True, verbose_name="Дата рождения"
+    )
+    gender = models.CharField(
+        max_length=25, choices=GENDER_CHOICES, blank=True, null=True, verbose_name="Пол"
+    )
+    photo = models.ImageField(
+        upload_to="users/%Y/%m/%d", blank=True, null=True, verbose_name="Аватар"
+    )
+    bio = models.TextField(
+        max_length=1000, blank=True, null=True, verbose_name="О себе"
+    )
+
+    def __str__(self):
+        return "Профиль пользователя: {}".format(self.user.username)
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+        ordering = ["-date_joined"]
