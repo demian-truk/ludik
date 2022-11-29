59ec12082f318c8657253274b1894b6031df17e1
Dima Teteruk <dimtruk93@gmail.com>
2022-11-29 22:49:48 +0300

    Add utils for cart

--- shop/utils.py
+++ shop/utils.py
@@ -1,6 +1,8 @@
+from django.db import models
 from django.db.models import Count
+from django.views.generic import View
 
-from .models import ProductsCategory
+from .models import Cart, Customer, ProductsCategory
 
 
 class ProductsCategoryDataMixin:
@@ -13,3 +15,29 @@ class ProductsCategoryDataMixin:
         if "category_selected" not in context:
             context["category_selected"] = 0
         return context
+
+
+class CartMixin(View):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            customer = Customer.objects.filter(user=request.user).first()
+            if not customer:
+                customer = Customer.objects.create(user=request.user)
+            cart = Cart.objects.filter(user=customer, in_order=False).first()
+            if not cart:
+                cart = Cart.objects.create(user=customer)
+        else:
+            cart = Cart.objects.filter(for_anonymous_user=True, in_order=False).first()
+            if not cart:
+                cart = Cart.objects.create(for_anonymous_user=True)
+        self.cart = cart
+        return super().dispatch(request, *args, **kwargs)
+
+
+def recalc_cart(cart):
+    cart_data = cart.products.aggregate(models.Sum("total_price"))
+    if cart_data.get("total_price__sum"):
+        cart.total_price = cart_data["total_price__sum"]
+    else:
+        cart.total_price = 0
+    cart.save()
