0b79d260391700f1e61a4fd41b786fc25d9b4293
Dima Teteruk <dimtruk93@gmail.com>
2022-11-29 18:32:47 +0300

    Add the "Product" and "ProductsCategory" models and register them in the admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Product, ProductsCategory
+
+
+@admin.register(ProductsCategory)
+class ProductsCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "get_html_image",
+        "price",
+        "stock",
+        "available",
+        "category",
+    )
+    list_display_links = ("id", "name")
+    search_fields = ("name", "category__name")
+    list_filter = ("available",)
+    list_editable = ("price", "stock", "available")
+    prepopulated_fields = {"slug": ("name",)}
+    readonly_fields = ("get_html_image",)
+
+    def get_html_image(self, object):
+        if object.image:
+            return mark_safe(f'<img src="{object.image.url}" width=60>')
+
+    get_html_image.short_description = "Превью"
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ProductsCategory(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название")
+    slug = models.CharField(
+        max_length=50, unique=True, db_index=True, verbose_name="Slug"
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("products_categories", kwargs={"category_slug": self.slug})
+
+    class Meta:
+        verbose_name = "Категория товаров"
+        verbose_name_plural = "Категории товаров"
+        ordering = ["name"]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Наименование")
+    slug = models.CharField(
+        max_length=255, unique=True, db_index=True, verbose_name="Slug"
+    )
+    category = models.ForeignKey(
+        ProductsCategory, on_delete=models.PROTECT, verbose_name="Категория"
+    )
+    image = models.ImageField(
+        upload_to="products/%Y/%m/%d/", verbose_name="Изображение"
+    )
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
+    stock = models.PositiveIntegerField(verbose_name="Остаток товара")
+    available = models.BooleanField(default=True, verbose_name="Доступность товара")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("show_product", kwargs={"product_slug": self.slug})
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["name"]
