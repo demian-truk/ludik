27ae0756575ef41fa6645dab1e43602fe5afba2c
Dima Teteruk <dimtruk93@gmail.com>
2022-11-29 22:41:06 +0300

    Add the "CartProduct", "Cart", "Customer", "Order" models and register them in the admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Product, ProductsCategory
+from .models import Cart, CartProduct, Customer, Order, Product, ProductsCategory
 
 
 @admin.register(ProductsCategory)
@@ -34,3 +34,41 @@ class ProductAdmin(admin.ModelAdmin):
             return mark_safe(f'<img src="{object.image.url}" width=60>')
 
     get_html_image.short_description = "Превью"
+
+
+@admin.register(CartProduct)
+class CartProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "cart")
+    list_display_links = ("id", "product")
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "in_order", "for_anonymous_user")
+    list_display_links = ("id", "user")
+    search_fields = ("user__username",)
+    list_filter = ("in_order", "for_anonymous_user")
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "phone", "address")
+    list_display_links = ("id", "user")
+    search_fields = ("user__username",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+        "address",
+        "time_create",
+        "status",
+    )
+    list_display_links = ("id", "first_name", "last_name")
+    search_fields = ("first_name", "last_name", "phone", "address")
+    list_filter = ("status", "time_create")
+    list_editable = ("status",)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
@@ -46,3 +47,110 @@ class Product(models.Model):
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
         ordering = ["name"]
+
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(
+        "Cart",
+        on_delete=models.CASCADE,
+        related_name="related_products",
+        verbose_name="Корзина",
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Товар")
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Количество")
+    total_price = models.DecimalField(
+        max_digits=8, decimal_places=2, verbose_name="Итоговая цена"
+    )
+
+    def __str__(self):
+        return "{} (для корзины №{})".format(self.product.name, self.cart.id)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.quantity * self.product.price
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Товар для корзины"
+        verbose_name_plural = "Товары для корзины"
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        "Customer", null=True, on_delete=models.CASCADE, verbose_name="Покупатель"
+    )
+    products = models.ManyToManyField(
+        CartProduct, related_name="related_cart", verbose_name="Товары"
+    )
+    in_order = models.BooleanField(default=False, verbose_name="В заказе")
+    for_anonymous_user = models.BooleanField(
+        default=False, verbose_name="Анонимный пользователь"
+    )
+    total_price = models.DecimalField(
+        max_digits=8, default=0, decimal_places=2, verbose_name="Итоговая цена"
+    )
+
+    def __str__(self):
+        return "Корзина №{}".format(self.id)
+
+    class Meta:
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзины"
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    phone = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name="Номер телефона"
+    )
+    address = models.CharField(
+        max_length=255, null=True, blank=True, verbose_name="Адрес"
+    )
+    orders = models.ManyToManyField(
+        "Order", related_name="related_customer", verbose_name="Заказы покупателя"
+    )
+
+    def __str__(self):
+        return "{}".format(self.user)
+
+    class Meta:
+        verbose_name = "Покупатель"
+        verbose_name_plural = "Покупатели"
+
+
+class Order(models.Model):
+    STATUS_NEW = "new"
+    STATUS_IN_PROGRESS = "in_progress"
+    STATUS_COMPLETED = "completed"
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, "Новый заказ"),
+        (STATUS_IN_PROGRESS, "В обработке"),
+        (STATUS_COMPLETED, "Заказ отправлен"),
+    )
+
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Корзина"
+    )
+    first_name = models.CharField(max_length=50, verbose_name="Имя")
+    last_name = models.CharField(max_length=50, verbose_name="Фамилия")
+    phone = models.CharField(max_length=20, verbose_name="Номер телефона")
+    address = models.CharField(max_length=255, verbose_name="Адрес доставки")
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default=STATUS_NEW,
+        verbose_name="Статус заказа",
+    )
+    comment = models.TextField(
+        max_length=1000, blank=True, null=True, verbose_name="Комментарий к заказу"
+    )
+    time_create = models.DateTimeField(auto_now=True, verbose_name="Время создания")
+
+    def __str__(self):
+        return "Заказ №{}".format(self.id)
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
